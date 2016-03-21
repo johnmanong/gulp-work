4e961f991eca76de07eecab68f268d0bf1f8151c
ong <ong@hioscar.com>
2016-03-21 18:46:47 -0400

    add command to compile down to single html file

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,13 @@ var webserver = require('gulp-webserver');
 var del = require('del');
 
 
+var replace = require('gulp-replace');
+var fs = require('fs');
+
+
 var paths = {
   src: {
+    root: 'app/index.html',
     scripts: ['app/js/**/*.js'],
     stylesheets: ['app/stylesheets/**/*.scss'],
   },
@@ -21,20 +26,21 @@ var paths = {
     stylesheets: {
       dir: 'app/build',
       name: 'index.css'
+    },
+    compiled: {
+      dir: 'app/build/compiled',
+      name: 'index.html'
     }
   }
 };
 
 // clean up dist
 gulp.task('clean', function() {
-  del(['app/build/*'], function (err, paths) {
-      console.log('Deleted files/folders:\n', paths.join('\n'));
-  });
-  return;
+  del.sync(['app/build/**/*', '!app/build'])
 });
 
 // sass build
-gulp.task('stylesheets', ['clean'], function() {
+gulp.task('stylesheets', function() {
   return gulp.src(paths.src.stylesheets)
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
@@ -43,7 +49,7 @@ gulp.task('stylesheets', ['clean'], function() {
 });
 
 // javascript build
-gulp.task('scripts', ['clean'], function() {
+gulp.task('scripts', function() {
   // Minify and copy all JavaScript (except vendor scripts)
   // with sourcemaps all the way down
   return gulp.src(paths.src.scripts)
@@ -74,10 +80,31 @@ function webserverTaskFactory(options) {
     }
 }
 
+// build compiled index.html file
+gulp.task('compile', ['clean', 'scripts', 'stylesheets'], function() {
+  return gulp.src(paths.src.root)
+    // inline stylesheets
+    .pipe(replace(/<link[^>]*href="([^\.]+\.css)"[^>]*>/g, function(s, filename) {
+      filename = ['./app', filename].join('/');
+      var style = fs.readFileSync(filename, 'utf8');
+      return '<style>\n' + style + '\n</style>';
+    }))
+    // inline js
+    .pipe(replace(/<script[^>]*src="(.+\.js)"[^>]*><\/script>/g, function(s, filename) {
+      filename = ['./app', filename].join('/');
+      var script = fs.readFileSync(filename, 'utf8');
+      return '<script>\n' + script + '\n</script>';
+    }))
+    .pipe(gulp.dest(paths.dist.compiled.dir))
+  });
+
 gulp.task('webserver', webserverTaskFactory());
 gulp.task('webserver:open', webserverTaskFactory({open: true}));
 
 // default task
-var defaultTasks = ['watch', 'scripts', 'stylesheets']
+var defaultTasks = ['watch', 'scripts', 'stylesheets'];
 gulp.task('default',  defaultTasks.concat(['webserver']));
 gulp.task('open', defaultTasks.concat(['webserver:open']));
+
+// build dist
+gulp.task('build-dist', ['compile']);
